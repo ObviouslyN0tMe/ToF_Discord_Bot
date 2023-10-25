c97a247ed9f75ac45ce5e3ca8f7ef651249579b8
Josua <josua@jeuck.de>
2023-10-26 00:00:29 +0200

    updated message content small improvements shortend/simplified code

--- Features/Trainingdates.py
+++ Features/Trainingdates.py
@@ -14,30 +14,30 @@ class Trainingdates(commands.Cog):
         await ctx.send("läuft")
 
     @commands.command()
-    async def neueWoche(self, ctx, monday):
+    async def neu(self, ctx, monday):
         # get monday date
         monday = monday.split(".")
         mon = datetime.date(int(monday[2]), int(monday[1]), int(monday[0]))
         # create all weekdates
         forward = datetime.timedelta(days=1)
-        w = {"mon": mon.strftime("%a %d.%m"),
-             "tue": (mon + 1*forward).strftime("%a %d.%m"),
-             "wed": (mon + 2*forward).strftime("%a %d.%m"),
-             "thu": (mon + 3*forward).strftime("%a %d.%m"),
-             "fri": (mon + 4*forward).strftime("%a %d.%m"),
-             "sat": (mon + 5*forward).strftime("%a %d.%m"),
-             "sun": (mon + 6*forward).strftime("%a %d.%m")}
+        w = {"mon": mon.strftime("**%a (%d. %B)**"),
+             "tue": (mon + 1*forward).strftime("**%a (%d. %B)**"),
+             "wed": (mon + 2*forward).strftime("**%a (%d. %B)**"),
+             "thu": (mon + 3*forward).strftime("**%a (%d. %B)**"),
+             "fri": (mon + 4*forward).strftime("**%a (%d. %B)**"),
+             "sat": (mon + 5*forward).strftime("**%a (%d. %B)**"),
+             "sun": (mon + 6*forward).strftime("**%a (%d. %B)**")}
         # create message headers
-        header_week = ":bar_chart:  TRAINING DATES  " + w["mon"] + " - " + w["fri"] + "\n"
-        header_weekend = ":bar_chart:  TRAINING DATES  " + w["sat"] + " - " + w["sun"] + "\n"
+        header_week = ":bar_chart:  **TRAINING DATES (8PM)**\n" + w["mon"] + " **-** " + w["fri"] + "\n"
+        header_weekend = ":bar_chart:  **TRAINING DATES**\n" + w["sat"] + " **-** " + w["sun"] + "\n"
         # create timeslots
-        dates_week = [emojis[0] + w["mon"] + " 8PM\n",
-                      emojis[1] + w["tue"] + " 8PM\n",
-                      emojis[2] + w["wed"] + " 8PM\n",
-                      emojis[3] + w["thu"] + " 8PM\n",
-                      emojis[4] + w["fri"] + " 8PM\n"]
-        dates_weekend = [emojis[0] + w["sat"] + " 3PM\n", emojis[1] + w["sat"] + " 8PM\n",
-                         emojis[2] + w["sun"] + " 3PM\n", emojis[3] + w["sun"] + " 8PM\n"]
+        dates_week = [emojis[0] + w["mon"] + "\n",
+                      emojis[1] + w["tue"] + "\n",
+                      emojis[2] + w["wed"] + "\n",
+                      emojis[3] + w["thu"] + "\n",
+                      emojis[4] + w["fri"] + "\n"]
+        dates_weekend = [emojis[0] + w["sat"] + " **3PM**\n", emojis[1] + w["sat"] + " **8PM**\n",
+                         emojis[2] + w["sun"] + " **3PM**\n", emojis[3] + w["sun"] + " **8PM**\n"]
         # create message for week
         message_week = header_week
         for timeslot in dates_week:
@@ -71,12 +71,11 @@ class Trainingdates(commands.Cog):
         if message.author.id == self.bot.user.id:
             # check old message content
             message_content = message.content.split("\n")
-            line_to_edit_index = 2*emojis.index(payload.emoji.name) + 2
-            line_to_edit = message_content[line_to_edit_index]
-            line_to_edit = line_to_edit.split(")")
+            line_to_edit_index = 2*emojis.index(payload.emoji.name) + 3
+            line_to_edit = message_content[line_to_edit_index].split(")", 1)
             # update playercount and player list
             playercount = str(int(line_to_edit[0].strip("(")) + 1)
-            line_to_edit[1] = line_to_edit[1].strip(" ")
+            line_to_edit[1] = line_to_edit[1].lstrip("  ")
             player_list = line_to_edit[1].split(", ")
             player_to_add = self.bot.get_user(payload.user_id)
             player_list.append(player_to_add.display_name)
@@ -86,12 +85,10 @@ class Trainingdates(commands.Cog):
                 player_list_str += player
                 player_list_str += ", "
             player_list_str = player_list_str.strip(", ")
-            line_edited = "(" + playercount + ") " + str(player_list_str)
-            message_content[line_to_edit_index] = line_edited
+            message_content[line_to_edit_index] = "(" + playercount + ")  " + str(player_list_str)
             new_msg_content = ""
             for line in message_content:
-                new_msg_content += line
-                new_msg_content += "\n"
+                new_msg_content += line + "\n"
             # send message
             await message.edit(content=new_msg_content)
 
@@ -106,27 +103,26 @@ class Trainingdates(commands.Cog):
         if message.author.id == self.bot.user.id:
             # check old message content
             message_content = message.content.split("\n")
-            line_to_edit_index = 2 * emojis.index(payload.emoji.name) + 2
-            line_to_edit = message_content[line_to_edit_index]
-            line_to_edit = line_to_edit.split(")")
-            # update playercount and player list
-            playercount = str(int(line_to_edit[0].strip("(")) + -1)
-            line_to_edit[1] = line_to_edit[1].strip(" ")
+            line_to_edit_index = 2 * emojis.index(payload.emoji.name) + 3
+            line_to_edit = message_content[line_to_edit_index].split(")", 1)
+            # update player list
+            line_to_edit[1] = line_to_edit[1].strip("  ")
             player_list = line_to_edit[1].split(", ")
             player_to_remove = self.bot.get_user(payload.user_id)
-            player_list.remove(player_to_remove.display_name)
-            # create new strings
+            if player_to_remove.display_name in player_list:
+                player_list.remove(player_to_remove.display_name)
             player_list_str = ""
             for player in player_list:
                 player_list_str += player
                 player_list_str += ", "
             player_list_str = player_list_str.strip(", ")
-            line_edited = "(" + playercount + ") " + str(player_list_str)
-            message_content[line_to_edit_index] = line_edited
+            # update playercount
+            playercount = str(len(player_list))
+            # create new message
+            message_content[line_to_edit_index] = "(" + playercount + ")  " + str(player_list_str)
             new_msg_content = ""
             for line in message_content:
-                new_msg_content += line
-                new_msg_content += "\n"
+                new_msg_content += line + "\n"
             # send message
             await message.edit(content=new_msg_content)
 
